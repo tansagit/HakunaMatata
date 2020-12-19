b49c71d7cfe094c9150e9c5b7ececc8d3d9e05d2
An <ttanan212@gmail.com>
2020-12-19 09:34:50 +0700

    update

--- HakunaMatata/Helpers/Constants.cs
+++ HakunaMatata/Helpers/Constants.cs
@@ -6,11 +6,7 @@
         public const decimal DEFAULT_LATITUDE = 16.0738013M;
         public const decimal DEFAULT_LONGTITUDE = 108.1477255M;
 
-        public static readonly string GOOGLE_MAP_API_KEY = "AIzaSyBY8PE_X6qdTWkZwdBENNpGJTYr1cTjG9I";
-
         public static readonly string GOOGLE_MAP_MARKER_API =
             "https://maps.googleapis.com/maps/api/js?key=AIzaSyBY8PE_X6qdTWkZwdBENNpGJTYr1cTjG9I&callback=initMap";
-
-        public static readonly string LOCAL_IMG_SERVER = "http://127.0.0.1:8887/";
     }
 }
